39345055da4e96cb84a8ecd0f079eac036e037bd
Levon Ter-Grigoryan <leonidusspartus@gmail.com>
2023-06-11 14:56:05 +0200

    vtable sample

---
+++ compilation/vtable/multi_derived.cpp
@@ -0,0 +1,17 @@
+class Mother {
+ public:
+  virtual void MotherMethod() {}
+  int mother_data;
+};
+
+class Father {
+ public:
+  virtual void FatherMethod() {}
+  int father_data;
+};
+
+class Child : public Mother, public Father {
+ public:
+  virtual void ChildMethod() {}
+  int child_data;
+};
